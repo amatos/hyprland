d12ef90de3ccd9989f78dcb35135a8a7c6f13dca
Alberth Matos <alberth@matos.cc>
2024-10-23 22:33:45 -0400

    Recent changes

--- scripts/wttr.py
+++ scripts/wttr.py
@@ -67,7 +67,7 @@ def format_time(time):
 
 
 def format_temp(temp):
-    return (hour['FeelsLikeC']+"°").ljust(3)
+    return (hour['FeelsLikeF']+"°").ljust(3)
 
 
 def format_chances(hour):
@@ -94,12 +94,12 @@ if tempint > 0 and tempint < 10:
     extrachar = '+'
 
 
-data['text'] = '<span font="10" rise="2000">'+WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-    "</span>"+extrachar+weather['current_condition'][0]['FeelsLikeC']+"°"
+data['text'] = '<span font="20" rise="2000">'+WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+    "</span>"+extrachar+weather['current_condition'][0]['FeelsLikeF']+"°"
 
-data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
-data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
-data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedKmph']}Km/h\n"
+data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_F']}°</b>\n"
+data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeF']}°\n"
+data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedMiles']}mph\n"
 data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
 for i, day in enumerate(weather['weather']):
     data['tooltip'] += f"\n<b>"
@@ -108,7 +108,7 @@ for i, day in enumerate(weather['weather']):
     if i == 1:
         data['tooltip'] += "Tomorrow, "
     data['tooltip'] += f"{day['date']}</b>\n"
-    data['tooltip'] += f"⬆️{day['maxtempC']}° ⬇️{day['mintempC']}° "
+    data['tooltip'] += f"⬆️{day['maxtempF']}° ⬇️{day['mintempF']}° "
     data['tooltip'] += f"🌅{day['astronomy'][0]['sunrise']} 🌇{day['astronomy'][0]['sunset']}\n"
     for hour in day['hourly']:
         if i == 0:
